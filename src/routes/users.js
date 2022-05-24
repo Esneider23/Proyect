cab9369e9b948c8e27dfc7a96f8677a5e5d2f215
Esneider23 <guzmane@utb.edu.co>
2022-05-24 14:48:29 -0500

    update

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,18 +7,6 @@ router.get('/signin', (req,res)=>{
     res.render('signin')
 })
 
-router.get('/signinC', (req,res)=>{
-    res.render('signinC')
-})
-
-
-router.get('/signupCl', (req,res)=>{
-    res.render('signupc');
-})
-
-router.get('/signupAd', isAuthenticated, (req,res)=>{
-    res.render('signup');
-})
 
 router.get('/logout', (req,res)=>
 {
