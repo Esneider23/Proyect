2ca0a6688724aab8ff64800a59cadf74cc76c1bf
Esneider23 <guzmane@utb.edu.co>
2022-05-31 01:48:06 -0500

    Jaider808

--- src/views/popup.js
+++ src/views/popup.js
@@ -1,4 +1,4 @@
-var btnAbrirPopup = document.getElementById('abrir-popup'),
+var btnAbrirPopup = document.getElementById('barranquilla'),
     overlay = document.getElementById('overlay'),
     popup = document.getElementById('popup'),
     btncerrarPopup = document.getElementById('btn-cerrar');
@@ -15,7 +15,7 @@ btncerrarPopup.addEventListener('click', function(){
 
 
 // 2
-var btnAbrirPopup2 = document.getElementById('abrir-popup2'),
+var btnAbrirPopup2 = document.getElementById('cali_'),
     overlay2 = document.getElementById('overlay2'),
     popup2 = document.getElementById('popup2'),
     btncerrarPopup2 = document.getElementById('btn-cerrar2');
@@ -32,7 +32,7 @@ btncerrarPopup2.addEventListener('click', function(){
 
 
 // 3
-var btnAbrirPopup3 = document.getElementById('abrir-popup3'),
+var btnAbrirPopup3 = document.getElementById('cartagena_'),
     overlay3 = document.getElementById('overlay3'),
     popup3 = document.getElementById('popup3'),
     btncerrarPopup3 = document.getElementById('btn-cerrar3');
@@ -49,7 +49,7 @@ btncerrarPopup3.addEventListener('click', function(){
 
 
 // 4
-var btnAbrirPopup4 = document.getElementById('abrir-popup4'),
+var btnAbrirPopup4 = document.getElementById('medellin_'),
     overlay4 = document.getElementById('overlay4'),
     popup4 = document.getElementById('popup4'),
     btncerrarPopup4 = document.getElementById('btn-cerrar4');
@@ -62,21 +62,4 @@ btnAbrirPopup4.addEventListener('click', function(){
 btncerrarPopup4.addEventListener('click', function(){
     overlay4.classList.remove('active');
     popup4.classList.remove('active');
-});
-
-
-// 5
-var btnAbrirPopup5 = document.getElementById('abrir-popup5'),
-    overlay5 = document.getElementById('overlay5'),
-    popup5 = document.getElementById('popup5'),
-    btncerrarPopup5 = document.getElementById('btn-cerrar5');
-
-btnAbrirPopup5.addEventListener('click', function(){
-    overlay5.classList.add('active');
-    popup5.classList.add('active');
-});
-
-btncerrarPopup5.addEventListener('click', function(){
-    overlay5.classList.remove('active');
-    popup5.classList.remove('active');
 });
