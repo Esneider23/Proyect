af0cbd802b423bd63beafc2c5529afed50f9af76
Esneider23 <guzmane@utb.edu.co>
2022-06-06 14:37:08 -0500

    update base online

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/proyecto')
+mongoose.connect('mongodb+srv://Admin:Admin@cluster0.ncv7l.mongodb.net/proyecto?retryWrites=true&w=majority')
 .then(db=>console.log("DB is connected"))
 .catch(error=>console.log(error))
